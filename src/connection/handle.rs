4249f397267d83617116f67f33e93d702e9a68b9
secondary-smiles <shavkinder@gmail.com>
2023-03-09 12:19:59 -0500

    Async server

--- src/connection/handle.rs
+++ src/connection/handle.rs
@@ -1,30 +1,28 @@
-use std::time::Duration;
-
 use async_std::io::prelude::*;
-use async_std::net::TcpStream;
-use async_std::task;
 
 // use async_std::prelude::*;
 use info_utils::prelude::*;
 
-pub async fn handle_connection(stream: &mut TcpStream) {
-    log!("{:?}", stream);
-    task::sleep(Duration::from_secs(2)).await;
+pub async fn handle_connection<T: Read + Write + Unpin>(stream: &mut T) {
+    let mut stream = stream;
+    
+    let header = read_header(&mut stream).await;
+    
+    let response = format!("HTTP/1.0 200 OK\r\n\r\n{}", header);
 
+    log!("{}", response);
+    stream.write(response.as_bytes()).await.eval();
+    stream.flush().await.eval();
+}
+
+async fn read_header<T: Read + Write + Unpin>(stream: &mut T) -> String {
     let mut data = vec![];
-    let mut buffer = [0; 4096];
+    let mut buffer = [0; 1024];
 
     let bytes_read = stream.read(&mut buffer).await.eval();
-
-    log!("read {} bytes", bytes_read);
-
     data.extend_from_slice(&buffer[..bytes_read]);
 
-    let data = String::from_utf8_lossy(&data);
+    let data = String::from_utf8_lossy(&data);    
 
-    log!("{}", data);
-
-    let response = format!("HTTP/1.0 200 OK\r\n\r\n{}", data);
-
-    stream.write(response.as_bytes()).await.eval();
+    data.to_string()
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use async_std::io::prelude::*;
+use async_std::net;
 use async_std::net::TcpListener;
 use futures::stream::StreamExt;
 
@@ -17,6 +19,8 @@ async fn main() {
         .for_each_concurrent(/*limit*/ None, |stream| async move {
             let mut stream = stream.eval();
             handle_connection(&mut stream).await;
+            stream.flush().await.eval();
+            stream.shutdown(net::Shutdown::Both).eval();
         })
         .await;
 }
